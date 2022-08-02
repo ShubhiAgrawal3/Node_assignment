63a2514b50188485c7232f51c2d55d4b453b53ee
gem-ShubhiAgrawal <Shubhi.Agrawal@Geminisolutions.com>
2022-08-02 17:31:04 +0530

    commit

--- models/employee.model.js
+++ models/employee.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 
 var employeeSchema = new mongoose.Schema({
     fullName: {
@@ -21,5 +23,7 @@ employeeSchema.path('email').validate((val) => {
     emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return emailRegex.test(val);
 }, 'Invalid e-mail.');
+
+
 
 mongoose.model('Employee', employeeSchema);
